f58a16b8a020518b6346b1a9ef1e491a595b5dd2
cesar <cesardyno0207@gmail.com>
2022-08-22 17:02:43 -0500

    se crea llave primaria y tabla costumers

---
+++ database.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    database="base1",
+   
+)
+
+mycursor = mydb.cursor()
+
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
+
+
+
+
+
