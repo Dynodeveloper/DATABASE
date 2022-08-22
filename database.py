bc260e6886ac9ce710d27e3c48bba9306ae75bd4
cesar <cesardyno0207@gmail.com>
2022-08-22 17:10:22 -0500

    actualizacion

--- database.py
+++ database.py
@@ -4,11 +4,15 @@ mydb = mysql.connector.connect(
     host='localhost',
     user='root',
     password='admin',
-    database="sys",
+    database="base1",
    
 )
 
 mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES") #muestra las bases de datos existentes
+
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") #se cambia la tabla "costumers", delegandola como llave primaria
 
 mycursor.execute("SHOW TABLES") #muestra las tabas de la base de datos
 
